a1cd33b74e1a03316a2087fe1a76f14123c5a2ef
Lorenzo Marchiori <kioli85@gmail.com>
2018-02-02 12:08:03 +0100

    Show passive form

--- App.js
+++ App.js
@@ -1,64 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   Platform,
   StyleSheet,
   Text,
   View,
-  Button,
-  NativeModules
-} from 'react-native';
+  Button
+} from 'react-native'
 
 var usabilla = require('usabilla-react-native')
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu',
+})
 
 export default class App extends Component<{}> {
-
   constructor() {
-    super();
-    usabilla.initialize(null);
-    usabilla.setFormDidLoadSuccessfully(this.onFormLoaded);
-    usabilla.setFormDidFailLoading((reminder) => console.log("Error loading form:", reminder));
-    usabilla.setFormDidCloase((reminder) => console.log(reminder.formId));
+    super()
+    usabilla.initialize(null)
+    usabilla.setFormDidLoadSuccessfully(this.onFormLoaded)
+    usabilla.setFormDidFailLoading((reminder) => console.log("Error loading form:", reminder))
+    usabilla.setFormDidClose((reminder) => console.log(reminder.formId))
   }
-
   onFormLoaded() {
-    usabilla.showLoadedForm();
+    usabilla.showLoadedForm()
   }
-
-  onShowForm() {
+  requestForm() {
     usabilla.loadFeedbackForm("5a744ccd8753b708620f8dd6")
   }
-
-  render() {    
+  render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-        <Button
-        onPress={this.onShowForm}
-        title="Show form"
-        />
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+        <Button onPress={this.requestForm} title="Form" />
       </View>
-    );
+    )
   }
 }
 
@@ -79,4 +57,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
--- android/app/src/main/java/com/rnusabilla/MainApplication.java
+++ android/app/src/main/java/com/rnusabilla/MainApplication.java
@@ -7,7 +7,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.reactnativepoc.BridgePackage;
+import com.usabilla.reactnative.ubform.BridgePackage;
 
 import java.util.Arrays;
 import java.util.List;
