75285929db1f086b3acf236cf2005561184eb0bb
Lorenzo Marchiori <lorenzo@usabilla.com>
2019-06-20 16:49:10 +0200

    fix(passive-form): Android add screenshot

--- UsabillaSampleApp/android/app/src/main/java/com/rnusabilla/MainActivity.java
+++ UsabillaSampleApp/android/app/src/main/java/com/rnusabilla/MainActivity.java
@@ -1,6 +1,10 @@
 package com.rnusabilla;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+
 import com.facebook.react.ReactFragmentActivity;
+import com.usabilla.reactnative.ubform.UsabillaBridge;
 
 public class MainActivity extends ReactFragmentActivity {
 
@@ -11,5 +15,19 @@ public class MainActivity extends ReactFragmentActivity {
     @Override
     protected String getMainComponentName() {
         return "RNUsabilla";
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        final Fragment fragment = getSupportFragmentManager().findFragmentByTag(UsabillaBridge.FRAGMENT_TAG);
+        // value 2 is REQUEST_CODE_PHOTO_PICK constant currently private in BaseForm.kt
+        fragment.onActivityResult(2, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        final Fragment fragment = getSupportFragmentManager().findFragmentByTag(UsabillaBridge.FRAGMENT_TAG);
+        // value 1 is REQUEST_CODE_PERMISSION constant currently private in BaseForm.kt
+        fragment.onRequestPermissionsResult(1, permissions, grantResults);
     }
 }
