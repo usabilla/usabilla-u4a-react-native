40431519642f44f58dd3a60507428261c643e8d5
Hitesh Jain <hitesh.jain@usabilla.com>
2019-12-09 18:17:18 +0100

    feat(ios): Add autolinking support

--- ios/ReactNativeUsabilla/ReactNativeUsabilla.m
+++ ios/ReactNativeUsabilla/ReactNativeUsabilla.m
@@ -6,116 +6,25 @@
 //  Copyright © 2018 Usabilla. All rights reserved.
 //
 
-#import "ReactNativeUsabilla.h"
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 
-@implementation ReactNativeUsabilla {
-    UINavigationController *loadedViewController;
-}
-
-- (instancetype)init
-{
-    self = [super init];
-    if (self) {
-        _usabillaInterface = [[UsabillaInterface alloc] init];
-        _usabillaInterface.delegate = self;
-    }
-    return self;
-}
-
-- (NSDictionary *)constantsToExport
-{
-    return @{ @"DEFAULT_DATA_MASKS":  [self.usabillaInterface getDefaultDataMasks]};
-}
+@interface RCT_EXTERN_MODULE(UsabillaInterface, NSObject)
 
 + (BOOL)requiresMainQueueSetup
 {
     return YES;
 }
 
-RCT_EXPORT_METHOD(dismiss)
-{
-    [self.usabillaInterface dismiss];
-}
-
-RCT_EXPORT_METHOD(setDataMasking:(NSArray<NSString *>*)masks : (NSString *)maskChar)
-{
-    [self.usabillaInterface setDataMasking:masks:maskChar];
-}
-
-RCT_EXPORT_MODULE(UsabillaBridge);
-
-- (NSArray<NSString *> *)supportedEvents
-{
-    return @[@"UBFormLoadingSucceeded",
-             @"UBFormLoadingFailed",
-             @"UBFormDidClose"];
-}
-
-RCT_EXPORT_METHOD(initialize:(NSString * _Nullable)appID)
-{
-    [self.usabillaInterface initialize:appID];
-}
-
-RCT_EXPORT_METHOD(loadFeedbackForm:(NSString * _Nonnull)formID)
-{
-    [self.usabillaInterface loadFeedbackForm:formID];
-}
-
-RCT_EXPORT_METHOD(loadFeedbackFormWithCurrentViewScreenshot:(NSString * _Nonnull)formID)
-{
-    dispatch_async(dispatch_get_main_queue(), ^{
-        UIViewController *rootController = (UIViewController *)[[[[UIApplication sharedApplication] delegate] window] rootViewController];
-        UIImage* screenshot = [self.usabillaInterface takeScreenshot:rootController.view];
-        [self.usabillaInterface loadFeedbackForm:formID screenshot:screenshot];
-    });
-}
-
-RCT_EXPORT_METHOD(setCustomVariables:(NSDictionary * _Nonnull)variables)
-{
-    [self.usabillaInterface setCustomVariables:variables];
-}
-
-RCT_EXPORT_METHOD(sendEvent:(NSString *)event)
-{
-    [self.usabillaInterface sendEvent:event];
-}
-
-RCT_EXPORT_METHOD(removeCachedForms)
-{
-    [self.usabillaInterface removeCachedForms];
-}
-
-RCT_EXPORT_METHOD(resetCampaignData:(RCTResponseSenderBlock)callback)
-{
-    [self.usabillaInterface resetCampaignData:^{
-        NSDictionary* resultsDict = @{@"success" : @YES};
-        callback(@[[NSNull null], resultsDict]);
-    }];
-}
-
-RCT_EXPORT_METHOD(showLoadedFrom)
-{
-    dispatch_sync(dispatch_get_main_queue(), ^{
-        UIViewController *rootController = (UIViewController *)[[[[UIApplication sharedApplication] delegate] window] rootViewController];
-        [rootController presentViewController:self->loadedViewController animated:true completion:nil];
-    });
-}
-
-#pragma mark UsabillaInterfaceDelegate Methods
-
--(void)formLoadedSucessfullyWithForm:(UINavigationController *)form {
-    self->loadedViewController = form;
-    [self sendEventWithName:@"UBFormLoadingSucceeded" body:@{@"success": @YES}];
-}
-
--(void)formFailedLoadingWithError:(NSError *)error {
-    self->loadedViewController = nil;
-    [self sendEventWithName:@"UBFormLoadingFailed" body:@{@"error": error.description}];
-}
-
--(void)formDidCloseWithFormID:(NSString *)formID withFeedbackResults:(NSArray<NSDictionary *> *)results isRedirectToAppStoreEnabled:(BOOL)isRedirectToAppStoreEnabled {
-    self->loadedViewController = nil;
-    [self sendEventWithName:@"UBFormDidClose" body:@{@"formId": formID, @"results": results, @"isRedirectToAppStoreEnabled": @(isRedirectToAppStoreEnabled)}];
-}
+RCT_EXTERN_METHOD(dismiss)
+RCT_EXTERN_METHOD(initialize:(NSString * _Nullable)appID)
+RCT_EXTERN_METHOD(loadFeedbackForm:(NSString * _Nullable)formID)
+RCT_EXTERN_METHOD(setDataMasking:(NSArray<NSString *>*)masks : (NSString *)maskChar)
+RCT_EXTERN_METHOD(setCustomVariables:(NSDictionary * _Nonnull)variables)
+RCT_EXTERN_METHOD(loadFeedbackFormWithCurrentViewScreenshot:(NSString * _Nonnull)formID)
+RCT_EXTERN_METHOD(sendEvent:(NSString *)event)
+RCT_EXTERN_METHOD(removeCachedForms)
+RCT_EXTERN_METHOD(resetCampaignData:(RCTResponseSenderBlock)callback)
 
 @end
