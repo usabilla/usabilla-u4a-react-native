6f38aa541062eab037577f46e0abefd42465b3aa
Hitesh Jain <hitesh.jain@usabilla.com>
2020-03-18 14:23:55 +0100

    feat(ios): Add support for load localisation via custom file

--- ios/ReactNativeUsabilla/ReactNativeUsabilla.m
+++ ios/ReactNativeUsabilla/ReactNativeUsabilla.m
@@ -19,6 +19,7 @@
 
 RCT_EXTERN_METHOD(dismiss)
 RCT_EXTERN_METHOD(initialize:(NSString * _Nullable)appID)
+RCT_EXTERN_METHOD(loadLocalizedStringFile:(NSString * _Nullable)localizedStringFile)
 RCT_EXTERN_METHOD(loadFeedbackForm:(NSString * _Nullable)formID)
 RCT_EXTERN_METHOD(setDataMasking:(NSArray<NSString *>*)masks : (NSString *)maskChar)
 RCT_EXTERN_METHOD(setCustomVariables:(NSDictionary * _Nonnull)variables)
