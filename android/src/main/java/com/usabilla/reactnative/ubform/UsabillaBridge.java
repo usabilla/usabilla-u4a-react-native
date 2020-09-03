af0693e0822875c0c764d365c320b5961b40e74c
Hitesh Jain <hitesh.jain@usabilla.com>
2020-09-03 11:17:26 +0200

    feat(android): Add support for androidx

--- android/src/main/java/com/usabilla/reactnative/ubform/UsabillaBridge.java
+++ android/src/main/java/com/usabilla/reactnative/ubform/UsabillaBridge.java
@@ -6,12 +6,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.content.LocalBroadcastManager;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
