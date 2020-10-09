d715f06b01d78352edc451b26b77bb8287350464
Hitesh Jain <hitesh.jain@usabilla.com>
2020-10-09 09:06:29 +0200

    fix(sample): Update android app

--- UsabillaSampleApp/__tests__/TestAddition.js
+++
@@ -1,7 +0,0 @@
-const sum = require('../usabilla-react-native')
-
-// Note: test renderer must be required after react-native.
-
-it('adds 1 + 2 to equal 3', () => {
-    expect(sum.sum(1, 2)).toBe(3);
-});
