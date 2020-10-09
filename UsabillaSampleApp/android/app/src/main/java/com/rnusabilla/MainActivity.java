0f239ae912064d3b289fe6971848f5ed37bc6be0
Hitesh Jain <hitesh.jain@usabilla.com>
2020-10-09 09:06:29 +0200

    fix(sample): Update android app

--- UsabillaSampleApp/android/app/src/main/java/com/rnusabilla/MainActivity.java
+++ UsabillaSampleApp/android/app/src/main/java/com/rnusabilla/MainActivity.java
@@ -1,33 +1,15 @@
 package com.rnusabilla;
 
-import android.content.Intent;
-import androidx.fragment.app.Fragment;
+import com.facebook.react.ReactActivity;
 
-import com.facebook.react.ReactFragmentActivity;
-import com.usabilla.reactnative.ubform.UsabillaBridge;
+public class MainActivity extends ReactActivity {
 
-public class MainActivity extends ReactFragmentActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "RNUsabilla";
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        final Fragment fragment = getSupportFragmentManager().findFragmentByTag(UsabillaBridge.FRAGMENT_TAG);
-        // value 2 is REQUEST_CODE_PHOTO_PICK constant currently private in BaseForm.kt
-        fragment.onActivityResult(2, resultCode, data);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        final Fragment fragment = getSupportFragmentManager().findFragmentByTag(UsabillaBridge.FRAGMENT_TAG);
-        // value 1 is REQUEST_CODE_PERMISSION constant currently private in BaseForm.kt
-        fragment.onRequestPermissionsResult(1, permissions, grantResults);
-    }
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "RNUsabilla";
+  }
 }
